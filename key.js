347a0034e2bacf4265797b1dbc953117abd65173
Francis Nepomuceno <francisnepo@gmail.com>
2019-12-06 13:44:00 -0500

    Added function for generating key from password

--- key.js
+++ key.js
@@ -1,3 +1,4 @@
+// Generates a public/private key pair
 async function generateEcKeypair() {
   let keypair = window.crypto.subtle.generateKey(
     {
@@ -9,6 +10,35 @@ async function generateEcKeypair() {
   )
 
   return keypair
+}
+
+// Generates a symmetric key from a given password
+async function generateKeyFromPassword(password) {
+  let enc = new TextEncoder()
+  let keyMaterial = await window.crypto.subtle.importKey(
+    'raw',
+    enc.encode(password),
+    { name: 'PBKDF2' },
+    false,
+    ['deriveBits', 'deriveKey']
+  )
+  let salt = window.crypto.getRandomValues(new Uint8Array(16))
+  let key = await window.crypto.subtle.deriveKey(
+    {
+      name: 'PBKDF2',
+      salt: salt,
+      iterations: 700000,
+      hash: 'SHA-256'
+    },
+    keyMaterial,
+    {
+      name: 'AES-GCM',
+      length: 256
+    },
+    true,
+    ['encrypt', 'decrypt']
+  )
+  return key
 }
 
 async function deriveSecretKey(privateKey, publicKey) {
